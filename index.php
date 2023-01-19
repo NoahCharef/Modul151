a9cf3ac1715319f62fdde4855e5f54b5e6838ae1
Noah Charef <noah.charef@ferag.com>
2023-01-19 08:45:55 +0100

    added initial db integration

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<php>
-    Hello World
-</php>
+<?php
+    $servername = "localhost";
+    $username = "db";
+    $password = "dbpass";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully\n";
+
+    $result = mysqli_query($conn, "SELECT * FROM schuldb.tbl_musikrichtung;");
+
+    foreach($result as $row)
+    {
+        echo $row["musikrichtung"];
+        echo "\n";
+    }
+?>
